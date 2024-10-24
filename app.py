d5c3c255cee4ea41c78c16cdfa3c73b71ad8686d
pelino250 <pmutanguha@alueducation.com>
2024-10-24 11:09:04 +0200

    project init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello DevOps!'
+
+@app.route('/error')
+def error():
+    abort(500, 'Opps something went wrong')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
